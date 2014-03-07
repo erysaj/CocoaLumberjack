4c70c28684f8ad43747a33b1db82ba62a23c0ad9
Eugene Rysaj <eugene@timecode.co>
2014-03-07 14:07:17 +0200

    r11176 (Clement): Commented out DDLog code that uses dispatch_get_current_queue because: - it crashes when called from a dispatch_async_block, like in [VideoRenderEx.mm stop] LOG(@"End stop procedure") - dispatch_get_current_queue is deprecated in iOS 6 SDK (see also: https://devforums.apple.com/thread/162529 ) - we aren't that interested in printing queue names anyway.

--- Lumberjack/DDLog.m
+++ Lumberjack/DDLog.m
@@ -868,10 +868,10 @@ static char *dd_str_copy(const char *str)
 		// If dispatch_get_current_queue() disappears, without a dispatch_queue_get_label() alternative,
 		// Apple will have effectively taken away our ability to properly log the name of executing dispatch queue.
 		
-		dispatch_queue_t currentQueue = dispatch_get_current_queue();
+//		dispatch_queue_t currentQueue = dispatch_get_current_queue();
 		#pragma clang diagnostic pop
 		
-		queueLabel = dd_str_copy(dispatch_queue_get_label(currentQueue));
+//		queueLabel = dd_str_copy(dispatch_queue_get_label(currentQueue));
 		
 		threadName = [[NSThread currentThread] name];
 	}
